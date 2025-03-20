aca23bd8b8af0469b5b199838f35d0e6c796983e
Ricardo Q. <ricardoqg41@icloud.com>
2025-03-19 21:08:15 -0500

    API pokemon color

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 
-
 export const getAllPokemon =async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1&offset=0');
     const data = await response.json();
     return data.results;
+}
+
+export const getColorPokemon = async (pokemon) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
+    const data = await response.json();
+    return data.color.name;
 }
